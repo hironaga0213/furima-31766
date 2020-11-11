d1dd90d34344cd4fce8f8c8ab03001d306dc47f6
hironaga0213 <72966985+hironaga0213@users.noreply.github.com>
2020-11-11 14:14:03 +0900

    修正2：userのバリデーション

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,14 +4,14 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  validates :nickname, presence: true
-  validates :first_name, presence: true
-  validates :family_name, presence: true
-  with_options presence: true, format: { with: /\A[ァ-ン]+\z/, message: 'Full-width katakana characters' } do
-    validates :read_first, presence: true
-    validates :read_family, presence: true
+  with_options presence: true do
+    validates :nickname, format: { with: /\A[ぁ-んァ-ン一-龥]/, message: "is invalid. Input full-width characters."}
+    validates :first_name, format: { with: /\A[ぁ-んァ-ン一-龥]/, message: "is invalid. Input full-width characters."}
+    validates :family_name, format: { with: /\A[ぁ-んァ-ン一-龥]/, message: "is invalid. Input full-width characters."}
+    validates :read_first, format: { with: /\A[ァ-ン]+\z/, message: 'Full-width katakana characters' }
+    validates :read_family, format: { with: /\A[ァ-ン]+\z/, message: 'Full-width katakana characters' }
+    validates :birth
   end
-  validates :birth, presence: true
 
   PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
   validates_format_of :password, with: PASSWORD_REGEX, message: 'Include both letters and numbers'
