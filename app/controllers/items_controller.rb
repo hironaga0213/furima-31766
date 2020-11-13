309b030e00a76aaac491e6a795e03b4612b68e8e
hironaga0213 <72966985+hironaga0213@users.noreply.github.com>
2020-11-13 11:33:52 +0900

    editアクションの編集

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -22,6 +22,12 @@ class ItemsController < ApplicationController
     @item = Item.find(params[:id])
   end
 
+  def edit
+    @item = Item.find(params[:id])
+  end
+
+  
+
   private
   def item_params
     params.require(:item).permit(:image, :title, :introduction, :category_id, :status_id, :delivery_fee_id, :prefecture_id, :delivery_time_id, :price).merge(user_id: current_user.id)
