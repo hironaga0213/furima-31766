c7a2622f8c53a5f9a145ba1d1a14aa56abf76735
hironaga0213 <72966985+hironaga0213@users.noreply.github.com>
2020-11-16 16:29:39 +0900

    コントローラー編集

--- app/controllers/purchases_controller.rb
+++ app/controllers/purchases_controller.rb
@@ -1,12 +1,17 @@
 class PurchasesController < ApplicationController
+  before_action :authenticate_user!
+  before_action :set_params
+
+  
   def index
     @purchase_address = PurchaseAddress.new
-    @item = Item.find(params[:item_id])
+    if (@item.purchase != nil) || (current_user.id == @item.user.id)
+      redirect_to root_path
+    end
   end
 
   def create
     @purchase_address = PurchaseAddress.new(purchase_params)
-    @item = Item.find(params[:item_id])
    if @purchase_address.valid?
     pay_item
     @purchase_address.save
@@ -19,6 +24,10 @@ class PurchasesController < ApplicationController
   private
   def purchase_params
     params.require(:purchase_address).permit(:postal_code, :prefecture_id, :municipality, :house_number, :building, :phone_number).merge(token: params[:token], user_id: current_user.id, item_id: params[:item_id])
+  end
+
+  def set_params
+    @item = Item.find(params[:item_id])
   end
 
   def pay_item
